6229f6bdd21393cbff4b9e4fdd15f948de07aa00
github username <github email address>
2018-08-22 13:31:30 -0500

    updated home_owner_to_user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :load_user
+
+  def home
+    @user = current_user  
+  end
+  
+  def new
+    @homeowners_associations = HomeownersAssociation.all
+  end
+
+  def create
+    @user = User.create(user_params)
+    @user.users_association_id 
+    redirect to '/home' # NEED TO REDIRECT OR LOG THEM IN
+  end
+
+  def show
+    
+  end
+
+  private
+
+  def load_user
+
+    if params[:id]
+      @user = User.find(params[:id])
+    else
+      @user = User.new
+    end
+  end
+
+  def user_params
+    require(:user).permit(:first_name, :last_name, :street, :city, :state, :zip, :email)
+  end
+end
