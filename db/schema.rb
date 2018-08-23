409e09be2bec7bbc4c8f695f0167ca7b273faabe
github username <github email address>
2018-08-23 18:45:56 -0500

    added seed data and posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_220212) do
+ActiveRecord::Schema.define(version: 2018_08_23_232115) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -47,6 +47,12 @@ ActiveRecord::Schema.define(version: 2018_08_20_220212) do
     t.datetime "updated_at", null: false
     t.index ["invoice_id"], name: "index_payments_on_invoice_id"
     t.index ["user_id"], name: "index_payments_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "text"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
